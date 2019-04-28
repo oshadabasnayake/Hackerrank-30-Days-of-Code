7cbf35a188427b19f284be2f1e0c0da66ff2920f
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-04-29 01:54:37 +0530

    1. Hello World
    Day 1

---
+++ 1. Hello World/1. Hello World.c
@@ -0,0 +1,18 @@
+//oshe ravanz contribution
+//oshada97
+//heckerrank
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    char input_string[100];
+    
+    scanf("%[^\n]s",&input_string);
+    printf("Hello, World.\n");
+    printf("%s",input_string);
+    return 0;
+}
